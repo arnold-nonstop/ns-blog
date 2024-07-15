842c2f08b9be01475f0392c2c57d953bc9bb993c
Arnold Parge <arnold.parge@nonstopio.com>
2024-07-15 12:50:11 +0000

    Feat: Showing only published posts on home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.order('created_at DESC')
+    @posts = Post.all.order('created_at DESC').where(status: 'published')
   end
 
   # GET /posts/1 or /posts/1.json
@@ -82,6 +82,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :status)
     end
 end
